1a11a2a1fe3e3d308fb9dcc5d70e152ef1dbe415
Azanul <azanulhaque@gmail.com>
2023-02-10 11:48:27 +0000

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ use rss::Channel;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// RSS url to fetch blogs from
-   #[arg(short, long)]
-   url: String,
+    /// RSS url to fetch blogs from
+    #[arg(short, long)]
+    url: String,
 
-   /// Number of times to greet
-   #[arg(short, long, default_value_t = 5)]
-   number_of_posts: u8,
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 5)]
+    number_of_posts: u8,
 }
 
 fn main() {
@@ -28,13 +28,29 @@ fn main() {
     let channel = Channel::read_from(&rss[..]).unwrap();
 
     let mut content = String::new();
-    for (_index, item) in channel.items().iter().enumerate().take(number_of_posts.into()) {
-        let pub_date = DateTime::parse_from_rfc2822(item.pub_date().unwrap()).unwrap().format("%Y-%m-%d").to_string();
-        content.push_str(&format!("[{}]({}) - {}\n\n", item.title().unwrap(), item.link().unwrap(), pub_date));
+    for (_index, item) in channel
+        .items()
+        .iter()
+        .enumerate()
+        .take(number_of_posts.into())
+    {
+        let pub_date = DateTime::parse_from_rfc2822(item.pub_date().unwrap())
+            .unwrap()
+            .format("%Y-%m-%d")
+            .to_string();
+        content.push_str(&format!(
+            "[{}]({}) - {}\n\n",
+            item.title().unwrap(),
+            item.link().unwrap(),
+            pub_date
+        ));
     }
 
     let re = Regex::new(r"(?s)<!-- blogs starts -->.*<!-- blogs ends -->").unwrap();
     let readme = std::fs::read_to_string("README.md").unwrap();
-    let new_readme = re.replace_all(&readme, &format!("<!-- blogs starts -->\n{}\n<!-- blogs ends -->", content));
+    let new_readme = re.replace_all(
+        &readme,
+        &format!("<!-- blogs starts -->\n{}\n<!-- blogs ends -->", content),
+    );
     std::fs::write("README.md", new_readme.as_bytes()).unwrap();
 }
